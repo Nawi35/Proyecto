5fa4b7f804807562bf83f53c7065890580ce8b6a
melitroncoso <68672246+melitroncoso@users.noreply.github.com>
2020-09-01 01:37:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,10 @@ int main()
     for(i = 0; i < 3; i++){
         scanf("%f", &grades[i]);
         total += grades[i]/3;
+	if(total >= 7)
+		printf("El alumnos esta aprobado.\n")
+	else
+		printf("El alumnos esta desaprobado.\n")
     }
     printf("El promedio es de %.2f.\n", total);
     return 0;
