53e50bf5b3bdc1f5cb36fc4416f3082c9d51cb67
melitroncoso <68672246+melitroncoso@users.noreply.github.com>
2020-09-01 01:53:09 -0300

    Borrar hola.txt

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 int main()
 {
     float grades[3], total = 0;
-    int i;
-    for(i = 0; i < 3; i++){
+    int i, j;	
+    for(j = 0; j < 50; j++)
+   for(i = 0; i < 3; i++){
         scanf("%f", &grades[i]);
         total += grades[i]/3;
 	if(total >= 7)
