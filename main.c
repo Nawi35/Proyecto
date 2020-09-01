d4ac1bdcaae271f91662a8ee5380a81696cf22cf
Nawi35 <68668030+Nawi35@users.noreply.github.com>
2020-09-01 02:25:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,25 +4,33 @@
 
 int main()
 {
-    float grades[3], classAverage = 0, average[50], standDev = 0;
+    int alumns;
+    float grades[3], classAverage = 0, standDev = 0;
     int i, j, approbed[3] = {0,0,0};
-    for(j = 0; j < 50; j++)
+    do{
+        printf("Ingrese la cantidad de alumnos en el curso.\n");
+        scanf("%d", &alumns);
+        if(!isDigit(alumns))
+            printf("Por favor, ingrese un n%cmero.\n", 163);
+    }while(!isDigit(alumns));
+    float average[alumns];
+    for(j = 0; j < alumns; j++)
         for(i = 0; i < 3; i++){
             scanf("%f", &grades[i]);
             average[j] += grades[i]/3;
-        if(total >= 7)
+        if(average[j] >= 7)
             approbed[0]++;
         else
-            if (total >= 4)
+            if (average[j] >= 4)
                 approbed[1]++;
             else
                 approbed[2]++;
         classAverage += average[i];
         }
-    for(i = 0; i < 50; i++){
-        standDev += Pow(average[i] - classAverage, 2)/n
+    for(i = 0; i < alumns; i++){
+        standDev += Pow(average[i] - classAverage, 2)/alumns;
     }
     printf("Aprobaron %d alumnos.\n%d se fueron a diciembre y %d a marzo.\n", approbed[0], approbed[1], approbed[2]);
-    printf("El promedio de todos los promedios es %f.\n", average / 50);
+    printf("El promedio de todos los promedios es %f. Su varianza es de %.2f, por lo que su desviación estándar de %.2f.\n", classAverage / alumns, pow(standDev,2), standDev);
     return 0;
 }
