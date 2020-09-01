744385178d8f6e408ae88e66c64acf8133230837
Nawi35 <68668030+Nawi35@users.noreply.github.com>
2020-09-01 02:09:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    float grades[3], total = 0, average = 0;
+    float grades[3], classAverage = 0, average[50], standDev = 0;
     int i, j, approbed[3] = {0,0,0};
     for(j = 0; j < 50; j++)
         for(i = 0; i < 3; i++){
             scanf("%f", &grades[i]);
-            total += grades[i]/3;
+            average[j] += grades[i]/3;
         if(total >= 7)
             approbed[0]++;
         else
@@ -16,10 +17,11 @@ int main()
                 approbed[1]++;
             else
                 approbed[2]++;
-	total = 0;
-	average += total;
+        classAverage += average[i];
         }
-
+    for(i = 0; i < 50; i++){
+        standDev += Pow(average[i] - classAverage, 2)/n
+    }
     printf("Aprobaron %d alumnos.\n%d se fueron a diciembre y %d a marzo.\n", approbed[0], approbed[1], approbed[2]);
     printf("El promedio de todos los promedios es %f.\n", average / 50);
     return 0;
