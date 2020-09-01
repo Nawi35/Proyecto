f5651db3686b66a5d03ac3340d48eedfb84b996d
Nawi35 <68668030+Nawi35@users.noreply.github.com>
2020-09-01 02:15:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,19 +10,19 @@ int main()
         for(i = 0; i < 3; i++){
             scanf("%f", &grades[i]);
             average[j] += grades[i]/3;
-        if(total >= 7)
+        if(average[j] >= 7)
             approbed[0]++;
         else
-            if (total >= 4)
+            if (average[j] >= 4)
                 approbed[1]++;
             else
                 approbed[2]++;
         classAverage += average[i];
         }
     for(i = 0; i < 50; i++){
-        standDev += Pow(average[i] - classAverage, 2)/n
+        standDev += pow(average[i] - classAverage, 2)/50;
     }
     printf("Aprobaron %d alumnos.\n%d se fueron a diciembre y %d a marzo.\n", approbed[0], approbed[1], approbed[2]);
-    printf("El promedio de todos los promedios es %f.\n", average / 50);
+    printf("El promedio de todos los promedios es %.2f. Su desviación estandar es de %.2f, por lo tanto su varianza de %.2f\n", classAverage, standDev, pow(standDev,2));
     return 0;
 }
